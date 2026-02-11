57bfdddb8afb314ec3174c7df6b92853323fa06b
Venkata Vinod Kumar Katakam <venkatavinod0896@gmail.com>
2026-02-11 23:47:05 +0530

    Update S3 bucket name for Terraform backend

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "vinod-terraform-state-bucket-0896"
+    bucket         = "vinod-terraform-state-bucket-0897"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "vinod-terraform-lock-table"
