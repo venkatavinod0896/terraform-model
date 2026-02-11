a53e7fbaa4e6804427bd9fbe6ae770ef6d78b562
Venkata Vinod Kumar Katakam <venkatavinod0896@gmail.com>
2026-02-11 23:49:58 +0530

    Update DynamoDB table name in backend configuration

--- environment/dev/backend.tf
+++ environment/dev/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "vinod-terraform-state-bucket-0897"
     key            = "dev/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "vinod-terraform-lock-table"
+    dynamodb_table = "vinodk-terraform-lock-table"
     encrypt        = true
   }
 }
